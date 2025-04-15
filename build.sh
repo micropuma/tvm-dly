fc62e10e61afad5d3b1683f8df68c1579fca29fc
micropuma <micropuma@163.com>
2025-04-15 20:09:41 +0800

    revise build script

--- build.sh
+++ build.sh
@@ -3,21 +3,23 @@ rm -rf build && mkdir build && cd build
 cp ../cmake/config.cmake .
 
 # controls default compilation flags (Candidates: Release, Debug, RelWithDebInfo)
-echo "set(CMAKE_BUILD_TYPE RelWithDebInfo)" >> config.cmake
+echo "set(CMAKE_EXPORT_COMPILE_COMMANDS ON)" >> config.cmake
+echo "set(CMAKE_BUILD_TYPE Debug)" >> config.cmake
 
 # LLVM is a must dependency for compiler end
-echo "set(USE_LLVM \"llvm-config --ignore-libllvm --link-static\")" >> config.cmake
+echo "set(USE_LLVM \"llvm-config --ignore-libllvm --link-shared\")" >> config.cmake
 echo "set(HIDE_PRIVATE_SYMBOLS ON)" >> config.cmake
 
 # GPU SDKs, turn on if needed
+echo "set(USE_PROFILER ON)" >> config.cmake
 echo "set(USE_CUDA   ON)" >> config.cmake
 echo "set(USE_METAL  OFF)" >> config.cmake
 echo "set(USE_VULKAN OFF)" >> config.cmake
 echo "set(USE_OPENCL OFF)" >> config.cmake
 
 # cuBLAS, cuDNN, cutlass support, turn on if needed
-echo "set(USE_CUBLAS OFF)" >> config.cmake
+echo "set(USE_CUBLAS ON)" >> config.cmake
 echo "set(USE_CUDNN  OFF)" >> config.cmake
 echo "set(USE_CUTLASS OFF)" >> config.cmake
 
-cmake .. -DLLVM_CONFIG_EXECUTABLE=/home/douliyang/local/LLVM/bin/llvm-config && cmake --build . --parallel $(nproc)
+cmake .. -DGTest_DIR=/home/douliyang/local/GTEST/lib/cmake/GTest && cmake --build . --parallel $(nproc)
